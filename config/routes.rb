afe9852021703a6b6c8464b009c7391b5045f24d
unknown <zhenka@zhenka-PC.(none)>
2012-09-07 21:02:09 -0400

    add some static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rt::Application.routes.draw do
   get "static_pages/home"
-
+  get "static_pages/about"
   get "static_pages/help"
-
-  get "static_pages/"
+  get "static_pages/contact"
 
   resources :microposts
 
